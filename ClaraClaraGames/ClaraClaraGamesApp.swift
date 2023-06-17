f6fe286812a974b96c38d20e72ce317967eb65f9
James Philhower <jamesphilhower@gmail.com>
2023-06-17 02:37:08 -0400

    Initial Commit

---
+++ ClaraClaraGames/ClaraClaraGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaraClaraGamesApp.swift
+//  ClaraClaraGames
+//
+//  Created by James Philhower on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ClaraClaraGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
